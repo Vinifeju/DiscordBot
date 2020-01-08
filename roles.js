e03f36430b4dbcc29becf0fa78c1144cbd4c7cd1
Vinifeju <50736501+Vinifeju@users.noreply.github.com>
2020-01-08 16:31:30 +0600

    Create roles.js

---
+++ roles.js
@@ -0,0 +1,20 @@
+let roles = {
+
+	'🐍' : 'Python dev',
+	'🐘' : 'PHP dev',
+	'🔖' : 'Front end',
+	'🌐' : 'Back end',
+	'🟧' : 'JavaScript',
+	'☕' : 'Java',
+	'💻' : 'C++',
+	'🖥️' : 'C#',
+	'💩' : '1C',
+	'🐦' : 'Swift',
+	'📱' : 'Kotlin',
+	'🟣' : 'Design',
+	'💠' : '3D-шник',
+	'👶' :'Начинающий'
+
+}
+
+module.exports = roles;
